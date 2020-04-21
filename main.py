704ff0485261d3257811ca4c22ef4dae8eca5a22
peppe1896 <giuseppe96parrotta@gmail.com>
2020-04-22 00:27:56 +0200

    MergeWork

--- main.py
+++ main.py
@@ -5,20 +5,14 @@ import numpy as np
 import insertion_sort as isort
 
 dim_array = 10
-array = [11, 3, 18, 16, 1]
-array_Asd = ac.create_rand_array(3)
-print("RANDOM ARRAY", array_Asd)
-print("random array ele", array_Asd[0])
-B = ac.create_ordered_array(dim_array)
-C = ac.create_inverted_array(dim_array)
+
 
 sdd = timer()
 #array = isort.insertion_sort(array)
-print(array)
 dds = timer()
 print("Tempo Insertion sort:", (dds - sdd), "secondi")
 
-arraio = np.array([3,2,1,4,7,5,6])
+arraio = [81, 16, 2, 1616, 69]
 print("arraio", arraio)
-msort.merge_sort(arraio, 0, len(arraio) -1 )
+msort.merge_sort(arraio, 0, len(arraio)-1)
 print(arraio)
--- merge_sort.py
+++ merge_sort.py
@@ -10,13 +10,12 @@ def get_sentinel(array):
 
 
 def merge(arr, p, q, r):
-    print("metgesord")
     L = arr[p:q]
-    R = arr[q:r]
+    R = arr[q:r + 1]
+    L.append(get_sentinel(arr))
+    R.append(get_sentinel(arr))
     i = j = 0
-    L = np.append(L, 1000)
-    R = np.append(R, 900)
-    for k in range(p, r):
+    for k in range(p, r + 1):
         if L[i] <= R[j]:
             arr[k] = L[i]
             i += 1
@@ -27,8 +26,46 @@ def merge(arr, p, q, r):
 
 def merge_sort(arr, p, r):
     if p < r:
-        q = (p + r)//2
+        q = (r+p) // 2
         merge_sort(arr, p, q)
-        merge_sort(arr, q+1, r)
+        merge_sort(arr, q + 1, r)
         merge(arr, p, q, r)
-    print("hello", arr)
+        print("MERGESORT", p, q, r)
+        print(arr)
+
+
+def merge_work(arr):  # online
+    if len(arr) > 1:
+        mid = len(arr) // 2  # Finding the mid of the array
+        L = arr[:mid]  # Dividing the array elements
+        R = arr[mid:]  # into 2 halves
+
+        merge_work(L)  # Sorting the first half
+        merge_work(R)  # Sorting the second half
+
+        i = j = k = 0
+
+        # Copy data to temp arrays L[] and R[]
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        # Checking if any element was left
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
+
+def MergeSort(A):
+    merge_sort(A, 0, len(A)-1)
