ec478c7dcfc4cc855cfa8ceb9d7a6ef1672cdca1
peppe1896 <giuseppe96parrotta@gmail.com>
2020-04-22 01:53:29 +0200

    Pre-test

--- main.py
+++ main.py
@@ -3,19 +3,25 @@ import array_creator as ac
 import merge_sort as msort
 import insertion_sort as isort
 
-dim_array = 10000
-ordered = ac.create_ordered_array(dim_array)
-tipo_input = "ORDINATO"
-# rand_array = ac.create_rand_array(dim_array, -dim_array, dim_array)
-print("N ", dim_array, " valori di INPUT")
+dim_array = 500
+array = ac.create_inverted_array(dim_array)
+tipo_input = "Invertito"  # Casuale Invertito Ordinato
+algoritmo = "InsertionSort"  # InsertionSort MergeSort
+
+
+print("N", dim_array, "valori di INPUT")
 start = timer()
-msort.MergeSort(ordered)
-#array = isort.insertion_sort(ordered)
+# msort.MergeSort(array)
+array = isort.insertion_sort(array)
 stop = timer()
 delta = stop - start
 print("Tempo Ins sort:", delta, "secondi. Input:", dim_array)
-nome_file = "ins_sort"+"--Ord--N="+str(dim_array)
-out = open(nome_file+".txt", "w")
-out.write("Algoritmo: INSERTION SORT.\n\nINPUT="+str(dim_array)+"\n\n"+"Tempo impiegato ad ordinare:" + str(delta)+"\n\nL'input è "+tipo_input)
+
+nome_file = "In="+str(dim_array)
+
+# NON TOCCARE
+
+out = open("./Test/"+tipo_input+"/"+algoritmo+"/"+nome_file+".txt", "w")
+out.write(str(delta)+"\n\nAlgoritmo: "+algoritmo+"\nDimensione Input:"+str(dim_array)+"\nTempo impiegato ad ordinare:" + str(delta)+"\nInput:"+tipo_input)
 
 out.close()
