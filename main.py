9d73268a2a8dc232e3cc5b35bf8fe91b16d27371
peppe1896 <giuseppe96parrotta@gmail.com>
2020-04-22 02:20:26 +0200

    Testati molti di insertion sort. Sto facendo quella casuale

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import array_creator as ac
 import merge_sort as msort
 import insertion_sort as isort
 
-dim_array = 500
-array = ac.create_inverted_array(dim_array)
-tipo_input = "Invertito"  # Casuale Invertito Ordinato
+dim_array = 100000
+array = ac.create_rand_array(dim_array)
+tipo_input = "Casuale"  # Casuale Invertito Ordinato
 algoritmo = "InsertionSort"  # InsertionSort MergeSort
 
-
+# NON TOCCARE
 print("N", dim_array, "valori di INPUT")
 start = timer()
 # msort.MergeSort(array)
@@ -18,10 +18,8 @@ delta = stop - start
 print("Tempo Ins sort:", delta, "secondi. Input:", dim_array)
 
 nome_file = "In="+str(dim_array)
-
-# NON TOCCARE
-
 out = open("./Test/"+tipo_input+"/"+algoritmo+"/"+nome_file+".txt", "w")
-out.write(str(delta)+"\n\nAlgoritmo: "+algoritmo+"\nDimensione Input:"+str(dim_array)+"\nTempo impiegato ad ordinare:" + str(delta)+"\nInput:"+tipo_input)
+out.write(str(delta)+"\n\nAlgoritmo: "+algoritmo+"\nDimensione Input:"+str(dim_array)+"\nTempo impiegato ad ordinare:" +
+          str(delta)+"\nInput:"+tipo_input)
 
 out.close()
